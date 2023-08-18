153692cf63b1b3921a1adc785a26bfcbcff014e4
Alexandr Bokov <bok.off@gmail.com>
2023-08-18 10:30:13 +0300

    check 5 (no validate)

--- controllers/cards.js
+++ controllers/cards.js
@@ -24,51 +24,45 @@ module.exports.getCard = (req, res) => {
 };
 
 module.exports.deleteCard = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card.findByIdAndRemove(req.params.cardId)
-      .then((card) => {
-        if (!card) {
-          res.status(404).send({ message: 'Карточка не найдена' });
-        } else res.status(200).send({ message: 'Карточка удалена' });
-      })
-      .catch(() => {
-        res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
-      });
-  } else { res.status(400).send({ message: 'Некоректно задан _id карточки' }); }
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Карточка не найдена' });
+      } else res.status(200).send({ message: 'Карточка удалена' });
+    })
+    .catch(() => {
+      res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
+    });
 };
 
 module.exports.addLike = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true },
-    )
-      .then((card) => {
-        if (!card) {
-          res.status(404).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' });
-        } else res.send(card);
-      })
-      .catch(() => {
-        res.status(404).send({ message: 'Передан несуществующий _id карточки' });
-      });
-  } else { res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' }); }
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' });
+      } else res.send(card);
+    })
+    .catch(() => {
+      res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+    });
 };
 
 module.exports.deleteLike = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } },
-      { new: true },
-    )
-      .then((card) => {
-        if (!card) {
-          res.status(404).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' });
-        } else res.send(card);
-      })
-      .catch(() => {
-        res.status(404).send({ message: 'Передан несуществующий _id карточки' });
-      });
-  } else { res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' }); }
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' });
+      } else res.send(card);
+    })
+    .catch(() => {
+      res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -21,21 +21,17 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUsersById = (req, res) => {
-  if (req.params.id.length === 24) {
-    const { id } = req.params;
-    User.findById(id)
-      .then((user) => {
-        if (!user) {
-          res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
-        }
-        res.status(200).send(user);
-      })
-      .catch(() => {
-        res.status(500).send({ message: 'На сервере произошла ошика' });
-      });
-  } else {
-    res.status(400).send({ message: 'Не верный формат id' });
-  }
+  const { id } = req.params;
+  User.findById(id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+      }
+      res.status(200).send(user);
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'На сервере произошла ошика' });
+    });
 };
 
 module.exports.refreshUserAvatar = (req, res) => {
